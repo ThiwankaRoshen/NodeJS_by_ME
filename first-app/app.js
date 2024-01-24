a568cb4100a9b6ee9cf937e4f20848e3877fcf54
ThiwankaRoshen <thiwanka.r.pathirana@gmail.com>
2024-01-24 17:00:27 +0530

    buit in module called path

--- first-app/app.js
+++ first-app/app.js
@@ -8,3 +8,8 @@ console.log('hey')
 var logger = require('./logger')
 
 logger('TRX')
+////////////////////////////////////
+//const use because for safety of mutability
+const path = require('path')
+var pathObj = path.parse(__filename)
+console.log(pathObj)
