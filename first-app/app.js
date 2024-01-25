0d53cc735195d6a808f080ea22592c680ad48b52
ThiwankaRoshen <thiwanka.r.pathirana@gmail.com>
2024-01-25 10:22:02 +0530

    now emitting can be catched using a class that is a sub class of  EventEmitter and this gives the ability to catch emit in imported file

--- first-app/app.js
+++ first-app/app.js
@@ -1,5 +1,5 @@
-const EventEmitter = require('events')
-const emitter = new EventEmitter()
+//const EventEmitter = require('events')
+// const emitter = new EventEmitter()
 
 // emitter.on('messageLogged', function(){
 //     console.log('Listener called')
@@ -12,10 +12,12 @@ const emitter = new EventEmitter()
 // emitter.on('messageLogged', function(arg){
 //     console.log('Listener called',arg)
 //  })
-                           //arrow notation 
-emitter.on('messageLogged', (arg)=>{
+const Logger = require('./logger')
+const logger = new Logger();
+                            //arrow notation 
+logger.on('messageLogged', (arg)=>{
     console.log('Listener called',arg)
 })
 
-const log = require('./logger')
-log('Trix')
+
+logger.log('Trix')
