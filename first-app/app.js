0aaece478afb317af46bb489cda319cf23dc4fa6
ThiwankaRoshen <thiwanka.r.pathirana@gmail.com>
2024-01-25 11:24:14 +0530

    added routes also to local 3000 when creating server

--- first-app/app.js
+++ first-app/app.js
@@ -1,11 +1,16 @@
 const http = require('http')
 
-const server = http.createServer()
-
-//when  you go to the link localhost:300 this get connection
-server.on('connection', (socket)=>{
-    console.log('new connection',socket)
+const server = http.createServer((req, res)=>{
+    if(req.url === '/'){
+        res.write('Hello World')
+        res.end()
+    }
+    if(req.url === '/api/courses'){
+        res.write(JSON.stringify([1,2,3]))
+        res.end()
+    }
 })
+
 server.listen(3000)
 console.log('lintening port 3000....')
 
